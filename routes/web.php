95467775e99e45004db3bc5c295a7bce61622f4b
Sandrey988 <andrey.sad8@yandex.by>
2018-10-17 09:20:32 +0300

    init admin panel

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(['prefix'=>'admin', 'namespace'=>'Admin','middleware'=>['auth']], function (){
+Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
